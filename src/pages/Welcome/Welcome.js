a10685ac5346fb2d2c2abf814fc00adb96f0f10b
Andres Felipe Larrotta Pino <andres.larrota@isobar.com>
2023-04-18 08:12:28 -0500

    feat(src): welcome page terminada con animaciones version desktop

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Button } from '../../components/Button/Button'
+import imageGame from '../../assets/title_welcome_page.png'
 
 export const Welcome = () => {
 
@@ -11,10 +12,20 @@ export const Welcome = () => {
                 <h1>Memory Game</h1>
                 <div className='welcome_image'>
                     <div className='welcome_image--square'></div>
-                    <div className='welcome_image--square'></div>
+                    <div className='welcome_image--square active'>
+                        <div className='welcome_image--square_container'>
+                            <div className='welcome_image--square-front'></div>
+                            <div className='welcome_image--square-back'>
+                                <img src={imageGame} alt='memory game logo' />
+                            </div>
+                        </div>
+                    </div>
                     <div className='welcome_image--square'></div>
                 </div>
                 <Button />
+            </div>
+            <div className='welcome_name'>
+                <p>Andres F. Larrotta Pino</p>
             </div>
         </div>
     )
